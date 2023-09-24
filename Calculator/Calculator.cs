c3dfead9dbc5a4db91a6d5de558e0ab34f9c4d5b
JaylaSPC <jaylaspc2006@gmail.com>
2023-09-23 21:21:36 -0500

    initial commit

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel;
+
+namespace ConsoleApp1;
+
+public class Calculator
+{
+    public static double Add(double x, double y)
+    {
+        return x + y;
+    }
+    
+    public static double Subtract(double x, double y)
+    {
+        return x - y;
+    }
+
+    public static double Multiply(double x, double y)
+    {
+        return x * y;
+    }
+
+    public static double Divide(double x, double y)
+    {
+        return x / y;
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.AccessControl;
+using Console = System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input 2 numbers: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            double sum = Calculator.Add(firstNumber, secondNumber);
+            double difference = Calculator.Subtract(firstNumber, secondNumber);
+            double product = Calculator.Multiply(firstNumber, secondNumber);
+            double quotient = Calculator.Divide(firstNumber, secondNumber);
+            
+            Console.WriteLine("Calculations:");
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {sum}");
+            Console.WriteLine($"{firstNumber} - {secondNumber} = {difference}");
+            Console.WriteLine($"{firstNumber} * {secondNumber} = {product}");
+            Console.WriteLine($"{firstNumber} / {secondNumber} = {quotient}");
+        }
+    }
+}
